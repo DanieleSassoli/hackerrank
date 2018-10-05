41a1f11c643110c07ef60cf5f3307605cdd80eeb
danielesassoli <daniele.sassoli@sky.uk>
2018-10-05 16:32:19 +0100

    Did fibonacci

---
+++ src/main/scala/Fibonacci.scala
@@ -0,0 +1,14 @@
+/*
+bit of a different implementation of fibonacci.
+I'm inserting elements at the beginning of the list instead than appending them at the end.
+I'm then adding the first 2 elements so that I don't have to traverse the whole list to get the last 2 items.
+ */
+
+def fibonacci(x: Int): Int = {
+  def go(l: List[Int]): Int = {
+    if (l.length == x) l.head
+    else go(l.take(2).sum :: l)
+  }
+  go(List(1, 0))
+}
+println(fibonacci(2))
